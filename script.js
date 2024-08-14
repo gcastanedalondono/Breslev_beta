7e8e026d1da9d0232a16ac1b0bfd7a7a5110bb70
vitavococo <gcastanedalondono@gmail.com>
2024-08-14 14:48:42 -0500

    metiendole JS al botón de whatsapp

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ function toggleMobileMenu () {
     mobileMenu.classList.toggle ('inactive');
 }
 
+document.getElementById('whatsapp_button').addEventListener('click', function() {
+    const phoneNumber = '+573046780036'; // Reemplaza con tu número de teléfono
+    const message = 'Cuentanos tus inquietudes, preguntas y dudas'; // Reemplaza con tu mensaje predefinido
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(whatsappURL, '_blank'); // Abre el enlace en una nueva pestaña
+});
+
 
