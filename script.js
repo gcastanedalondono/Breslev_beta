0036fdf3f3065fe1e0dc2b0f790d99b0ac376d67
vitavococo <gcastanedalondono@gmail.com>
2024-08-18 14:29:23 -0500

    Porfin dio el whatsapp

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ function toggleMobileMenu () {
 
 
 
-
+document.getElementById('whatsapp_button').addEventListener('click', function() {
+    const phoneNumber = '+573046780036'; // Reemplaza con tu número de teléfono
+    const message = 'Cuentanos tus inquietudes, preguntas y dudas'; // Reemplaza con tu mensaje predefinido
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(whatsappURL, '_blank'); // Abre el enlace en una nueva pestaña
+});
