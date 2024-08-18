12bae954c547215a2752d0a5970a39bb6d4d1491
vitavococo <gcastanedalondono@gmail.com>
2024-08-18 14:21:35 -0500

    Cuadro el menu en mobile y el logo

--- script.js
+++ script.js
@@ -6,24 +6,6 @@ function toggleMobileMenu () {
     mobileMenu.classList.toggle ('inactive');
 }
 
-const whatapp_icon = document.querySelectorAll('.whatsapp_button');
-
-whatapp_icon.forEach(icono =>)
-
-.addEventListener('click', function() {
-    const phoneNumber = '+573046780036'; // Reemplaza con tu número de teléfono
-    const message = 'Cuentanos tus inquietudes, preguntas y dudas'; // Reemplaza con tu mensaje predefinido
-    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.open(whatsappURL, '_blank'); // Abre el enlace en una nueva pestaña
-});
 
 
-const iconos = document.querySelectorAll('.icono');
 
-iconos.forEach(icono => {
-    // Aquí puedes aplicar la función o el evento que desees
-    icono.addEventListener('click', () => {
-        // Acción que quieres realizar cuando el icono es clickeado
-        console.log('Icono clickeado');
-    });
-});
