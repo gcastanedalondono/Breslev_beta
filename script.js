2923d6d95ea0106eaec7c0ae0135b358129b193f
vitavococo <gcastanedalondono@gmail.com>
2024-08-18 20:51:27 -0500

    empiezan a funcionar los botones de whatsapp en diferentes páginas!

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function toggleMobileMenu () {
     mobileMenu.classList.toggle ('inactive');
 }
 
-
-
-document.getElementById('whatsapp_button').addEventListener('click', function() {
-    const phoneNumber = '+573046780036'; // Reemplaza con tu número de teléfono
-    const message = 'Cuentanos tus inquietudes, preguntas y dudas'; // Reemplaza con tu mensaje predefinido
-    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.open(whatsappURL, '_blank'); // Abre el enlace en una nueva pestaña
+document.addEventListener('DOMContentLoaded', function() {
+    var whatsappButtons = document.querySelectorAll('.whatsapp-btn');
+    whatsappButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            window.open('https://wa.me/1234567890', '_blank'); // Reemplaza con tu número de WhatsApp
+        });
+    });
 });
+
