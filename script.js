483d365db577d619dfe9cf66c7f803aafbd81f01
vitavococo <gcastanedalondono@gmail.com>
2024-08-18 21:12:39 -0500

    Todos los botones quedaron funcionando en la tienda, estilelos y después los copiamos en todos los archivos

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var youtubeButtons = document.querySelectorAll('.youtube-btn');
+    var youtubeChannelUrl = 'https://www.youtube.com/@asociacionbreslevcolombia24/videos'; // Reemplaza con la URL de tu canal de YouTube
+
+    youtubeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            window.open(youtubeChannelUrl, '_blank');
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var facebookButtons = document.querySelectorAll('.facebook-btn');
+    var facebookProfileUrl = 'https://www.facebook.com/search/top?q=asociacion%20breslev%20colombia'; // Reemplaza con la URL de tu perfil de Facebook
+
+    facebookButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            window.open(facebookProfileUrl, '_blank');
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var soundcloudButtons = document.querySelectorAll('.soundcloud-btn');
+    var soundcloudProfileUrl = 'https://soundcloud.com/asociacion-breslev-colombia'; // Reemplaza con la URL de tu perfil de SoundCloud
+
+    soundcloudButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            window.open(soundcloudProfileUrl, '_blank');
+        });
+    });
+});
